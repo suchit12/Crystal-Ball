e20be3523d28a19fbca95b45d59011698b32c6c9
Suchit <guestbandaram@gmail.com>
2017-08-13 12:10:33 -0700

    Making change

--- createtable.sql
+++ createtable.sql
@@ -10,5 +10,15 @@ CREATE TABLE(
 
 INSERT INTO production_servers (ServerName, 
 	Application, Version, PreviousUpdate) VALUES 
-	'prearchiver02-02.usw', 'Java', '4.5.2', '2015-06-07' );
+	('prearchiver02-02.usw', 'Java', '4.5.2', '2015-06-07'), 
+	('c05-escold02-02.sv4', 'Python', '8.6.4', '2016-05-04')
+	('c05-escold02-03.sv4', 'Java', '6.7.8', '2017-08-24' )
+	('prearchiver01-usw2','PHP',' 6.7.2 ','2014-09-25 ')
+	('prearchiver02-usw2 ','PHP ','7.8.3 ','2015-03-22 ')
+	('prearchiver04-usw2 ','MySql',' 5.3.2 ','2015-09-15')
+	('c07-escold06-02.sv4',' Python ','8.1.2 ','2015-09-01')
+	('c07-escold06-01.sv4' ,'Python' ,'5.6.3' ,'2015-02-02')
+	('c05-swriter01.sv4' ,'PHP' ,'4.5.2' ,'2016-05-09')
+	('c05-escold04-02.sv4','Java', '7.1.2', '2017-06-13')
+;
 
