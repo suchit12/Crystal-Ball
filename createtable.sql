07c2cf81a461e5f6be4fb8c6d68a7e4297186237
Suchit <guestbandaram@gmail.com>
2017-08-13 10:51:07 -0700

    asf

---
+++ createtable.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE production_machines;
+
+CREATE TABLE(
+	ServerName varchar(40),
+	Application varchar(40),
+	Version int(40),
+	PreviousUpdate date
+	
+);
+
+INSERT INTO
--- getfile.php
+++ getfile.php
@@ -1,35 +1,33 @@
 <?php
 
-	$file = file("serverentries.txt");
-	$file1 = file("deletedentries.txt");
+	$file = file("serverentries.txt"); //adds on to file serverentries.txt
 	
-	require_once('./mysqli_connect.php');
+	require_once('./mysqli_connect.php'); //uses mysqli_connect to connect to database
 
 	$con = mysqli_connect('localhost','root','manchester2002');
 	mysqli_select_db($con, 'production_machines');
 
-	$sql= "DELETE FROM production_servers";
+	$sql= "DELETE FROM production_servers"; //Deletes all from database before adding on to the database data
 	$q = "";
 	
 	if(mysqli_query($con, $sql)) {
 		foreach($file as $newline) {
-			foreach($file1 as $line1){
-				$newline1 = rtrim($newline, " ");
-				$line = rtrim($line1, " \t.");
-				if($line != $newline1){
-					echo $line1 . '<br>';
-					echo $newline . '<br>';
-					$a = explode(" ", $newline);
-					$server = $a[0];
-					$app = $a[1];
-					$pack = $a[2];
-					$date = $a[3];
-					
-					$sql = "INSERT INTO production_servers (ServerName, 
-					Application, Version, PreviousUpdate) VALUES 
-					('$server', '$app', '$pack', '$date' )";
-					mysqli_query($con, $sql);
-				}
+			$newline1 = rtrim($newline, " "); //trims of spaces between servername, application, version, and previousupdate
+			$line = rtrim($line1, " \t.");
+			if($line != $newline1){
+				echo $line1 . '<br>';
+				echo $newline . '<br>';
+				$a = explode(" ", $newline);
+				$server = $a[0];
+				$app = $a[1];
+				$pack = $a[2];
+				$date = $a[3];
+				
+				//inserts all the values into database using query and for loop	
+				$sql = "INSERT INTO production_servers (ServerName, 
+				Application, Version, PreviousUpdate) VALUES 
+				('$server', '$app', '$pack', '$date' )";
+				mysqli_query($con, $sql);
 			}
 		}
 	}
